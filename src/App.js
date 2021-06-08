4fc47e2b018b57d406df52a967610089f14cbe4b
Artem Chernenko <fantom96fantom96@outlook.com>
2021-06-08 18:26:50 +0300

    fix

--- src/App.js
+++ src/App.js
@@ -1,23 +1,38 @@
-import logo from './logo.svg';
 import './App.css';
+import search from './images/search.svg'
+import heart from './images/heart.svg'
+import {cards} from './mocData/index'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header className="header">
+          <div className="header__img">
+              <img src={search}  className="header__search-img"/>
+          </div>
+          <input className="header__search" placeholder="Search..."/>
       </header>
+        <main className="main">
+            {
+                cards.map((card,index) => {
+                    return (
+                        <div className="card" key={index}>
+                            <div className="heart">
+                                <img src={heart}/>
+                            </div>
+                            <div className="card__img">
+                                <img src={card.img}/>
+                            </div>
+                            <div className="card__description">
+                                <div className="brand">{card.name}</div>
+                                <div className="brand-2">{card.name}</div>
+                            </div>
+                        </div>
+                    )
+                })
+            }
+
+        </main>
     </div>
   );
 }
---
+++ src/mocData/index.js
@@ -0,0 +1,29 @@
+export const cards = [
+    {   name : 'Hourglass Jeans',
+        img: 'https://via.placeholder.com/150'
+    },
+    {   name : 'Clover Clothing Co.',
+        img: 'https://via.placeholder.com/150'
+    },
+    {   name : 'Runway Lingerie',
+        img: 'https://via.placeholder.com/150'
+    },
+    {   name : 'Porcelain Apparel',
+        img: 'https://via.placeholder.com/150'
+    },
+    {   name : 'Forester Flannels',
+        img: 'https://via.placeholder.com/150'
+    },
+    {   name : 'Finer Threads',
+        img: 'https://via.placeholder.com/150'
+    },
+    {   name : 'Lil’ Boss Jean Co.',
+        img: 'https://via.placeholder.com/150'
+    },
+    {   name : 'Wicked Stitch',
+        img: 'https://via.placeholder.com/150'
+    },
+    {   name : 'Tanked Up Co.',
+        img: 'https://via.placeholder.com/150'
+    },
+    ]
